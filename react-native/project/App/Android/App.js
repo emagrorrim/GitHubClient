9a0cb9933f259afc8b4f9cb453ac06b5b59addd8
Xin Guo <emagrorrimxguo@gmail.com>
2018-06-12 10:06:33 +0800

    Split Android and iOS UI entry

---
+++ react-native/project/App/Android/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Text>This is Android!</Text>
+    );
+  }
+}
--- react-native/project/index.js
+++ react-native/project/index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App/App';
+import { 
+  AppRegistry,
+  Platform
+} from 'react-native'
+import iOSApp from './App/iOS/App'
+import AndroidApp from './App/Android/App'
 
-AppRegistry.registerComponent('project', () => App);
+AppRegistry.registerComponent('project', () => {
+  switch (Platform.OS) {
+    case "ios":
+      return iOSApp
+    case "android":
+      return AndroidApp
+    default:
+      return undefined
+  }
+})
