4335d3378959603475dc906201b90bb2ad24f65c
Xin Guo <emagrorrimxguo@gmail.com>
2018-06-28 22:37:17 +0800

    [ios] Add profile vc

---
+++ ios/StartKit/StartKit/GitHub/Profile/View/GitHubProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubProfileViewController.swift
+//  StartKit
+//
+//  Created by Xin Guo  on 2018/6/28.
+//  Copyright © 2018 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class GitHubProfileViewController: UIViewController {
+  
+}
